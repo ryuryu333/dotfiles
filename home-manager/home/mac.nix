c142b655ccd67d8dfd876b270c208c1215fdd166
ryuryu333 <risarisa062518@gmail.com>
2025-11-30 22:58:53 +0900

    mac 用の環境を記述

--- home-manager/home/mac.nix
+++ home-manager/home/mac.nix
@@ -3,4 +3,14 @@
 {
   home.username = "ryu";
   home.homeDirectory = "/Users/ryu";
+  nixpkgs.config.allowUnfree = true;
+  home.packages = with pkgs; [
+    zsh
+    vscode
+  ];
+
+  home.file = {
+    # ".zshrc".source = ../zsh/.zshrc;
+    # ".profile".source = ../zsh/.profile;
+  };
 }
