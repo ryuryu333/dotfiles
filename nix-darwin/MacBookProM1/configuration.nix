d1f5892d6f6961f8b4a10025d2d3594231a4d9b1
ryuryu333 <risarisa062518@gmail.com>
2026-01-11 23:41:28 +0900

    nix darwin を適用

--- nix-darwin/MacBookProM1/configuration.nix
+++ nix-darwin/MacBookProM1/configuration.nix
@@ -1,10 +1,16 @@
 {
   pkgs,
+  self,
   ...
 }:
 
 {
+  nixpkgs.hostPlatform = "aarch64-darwin";
   system.stateVersion = 6;
   system.configurationRevision = self.rev or self.dirtyRev or null;
-  environment.etc."nix-darwin/flake.nix".source = ./flake.nix;
+  # environment.systemPackages =
+  #   [ pkgs.vim
+  #   ];
+  # avoid conflicts with Determinate-Nix
+  nix.enable = false;
 }
