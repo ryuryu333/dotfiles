ae44b54995ff896ba86bb0eb26e8506b805f06a9
ryuryu333 <risarisa062518@gmail.com>
2026-03-15 13:01:48 +0900

    nix-versions の inputs をモジュール側へ伝播

--- nix-darwin/MacBookProM1/home_manager.nix
+++ nix-darwin/MacBookProM1/home_manager.nix
@@ -1,10 +1,14 @@
 {
   user,
+  nix-versions,
   ...
 }:
 {
   home-manager.useGlobalPkgs = true;
   home-manager.useUserPackages = true;
+  home-manager.extraSpecialArgs = {
+    inherit nix-versions;
+  };
   home-manager.users.${user} = {
     imports = [
       ../../home-manager/home/common.nix
