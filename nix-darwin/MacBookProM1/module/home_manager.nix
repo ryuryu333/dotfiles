6ec045f0b428255cd3d88e7dff217edbb63c7ef3
ryuryu333 <risarisa062518@gmail.com>
2026-07-01 16:55:57 +0900

    パス更新

--- nix-darwin/MacBookProM1/module/home_manager.nix
+++ nix-darwin/MacBookProM1/module/home_manager.nix
@@ -11,8 +11,8 @@
   };
   home-manager.users.${user} = {
     imports = [
-      ../../home-manager/common.nix
-      ../../home-manager/mac.nix
+      ../../../home-manager/common.nix
+      ../../../home-manager/mac.nix
     ];
   };
 }
