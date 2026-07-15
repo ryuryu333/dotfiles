4f3617a56733a3d3d42c3c174f87a80be913574c
ryuryu333 <risarisa062518@gmail.com>
2026-07-15 13:00:45 +0900

    bash の設定ファイルが冗長だったので変更

--- home-manager/wsl.nix
+++ home-manager/wsl.nix
@@ -14,18 +14,12 @@
   ];
 
   # 将来的には system manager で起動シェルを zsh にしたい
-  xdg.configFile."bash/profile".text = ''
+  home.file.".profile".text = ''
     if [[ $- == *i* ]] \
       && [[ "''${SHLVL:-0}" -eq 1 ]] \
       && [[ -n "''${WSL_DISTRO_NAME:-}" ]] \
       && [[ -x "$HOME/.nix-profile/bin/zsh" ]]; then
       exec "$HOME/.nix-profile/bin/zsh"
-    fi
-  '';
-
-  home.file.".profile".text = ''
-    if [ -n "''${BASH_VERSION:-}" ]; then
-      source "''${XDG_CONFIG_HOME:-$HOME/.config}/bash/profile"
     fi
   '';
   
