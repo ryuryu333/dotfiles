3274a5126bfb4e50742fb3e96102c7833abb8f83
ryuryu333 <risarisa062518@gmail.com>
2026-07-15 17:08:04 +0900

    設定を分割

--- home-manager/default.nix
+++ home-manager/default.nix
@@ -1,29 +1,8 @@
 { config, pkgs, nix-versions, ... }:
-
 {
-  programs.home-manager.enable = true;
-  home.stateVersion = "26.05";
-
-  xdg.enable = true;
-
-  home.packages = with pkgs; [
-    nixfmt
-    tree # ディレクトリ構造を表示
-    go-task # task runner
-    jq # JSONデータを操作
-  ]
-  ++ [
-    nix-versions.packages.${pkgs.stdenv.hostPlatform.system}.nix-versions
-  ];
-
   imports = [
-    ../app-config
+    ./module/base.nix
+    ./module/nixconf.nix
+    ./module/app.nix
   ];
-
-  nix.registry = {
-    templates = {
-      from = { type = "indirect"; id = "templates"; };
-      to = { type = "github"; owner = "ryuryu333"; repo = "nix_template"; };
-    };
-  };
 }
