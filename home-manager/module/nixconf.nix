bf673f9df7dd99210884710de6e7d39ea69b38e4
ryuryu333 <risarisa062518@gmail.com>
2026-07-16 00:32:50 +0900

    nix 設定を system manager 側に記述、手動で nix conf custom 削除

--- home-manager/module/nixconf.nix
+++ home-manager/module/nixconf.nix
@@ -1,14 +1,4 @@
-{ pkgs, ... }:
 {
-  nix.package = pkgs.nix;
-
-  nix.settings = {
-    extra-substituters = [ "https://cache.numtide.com" ];
-    extra-trusted-public-keys = [
-      "niks3.numtide.com-1:DTx8wZduET09hRmMtKdQDxNNthLQETkc/yaX7M4qK0g="
-    ];
-  };
-
   nix.registry = {
     templates = {
       from = {
