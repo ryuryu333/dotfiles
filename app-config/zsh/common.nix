1881aa1bd82eb9db9fa713410c258db690c20b54
ryuryu333 <risarisa062518@gmail.com>
2026-07-15 12:56:53 +0900

    zsh 設定追加

--- app-config/zsh/common.nix
+++ app-config/zsh/common.nix
@@ -1,11 +1,13 @@
 {
   programs.zsh = {
     enable = true;
+    enableCompletion = true;
     autosuggestion.enable = true;
     oh-my-zsh = {
       enable = true;
       plugins = [
         "safe-paste"
+        "sudo"
       ];
       custom = "${./ohmyzsh}";
       theme = "mytheme";
