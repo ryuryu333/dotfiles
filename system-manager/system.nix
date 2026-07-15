e2b6bda6a152fca98f11807becaea5ad22e2fd4f
ryuryu333 <risarisa062518@gmail.com>
2026-07-16 00:51:50 +0900

    メモ追加

--- system-manager/system.nix
+++ system-manager/system.nix
@@ -16,6 +16,8 @@
 
       # Add directories and files to /etc and set their permissions.
       etc = {
+        # 初回導入時、Nix Installerが生成した空の既存ファイルは手動で削除した。
+        # 設定変更後は `sudo systemctl restart nix-daemon.service` で再読み込みする。
         "nix/nix.custom.conf".text = ''
           extra-substituters = https://cache.numtide.com
           extra-trusted-public-keys = niks3.numtide.com-1:DTx8wZduET09hRmMtKdQDxNNthLQETkc/yaX7M4qK0g=
