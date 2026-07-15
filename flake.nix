74f626edcdda80ba9fa10144641c536cf866e6a6
ryuryu333 <risarisa062518@gmail.com>
2026-07-15 21:24:54 +0900

    check 追加

--- flake.nix
+++ flake.nix
@@ -81,5 +81,13 @@
           ];
         };
       };
+
+      checks = {
+        x86_64-linux.wsl =
+          self.homeConfigurations."ryu@main".activationPackage;
+
+        aarch64-darwin.mac =
+          self.darwinConfigurations.MacBook.system;
+      };
     };
 }
