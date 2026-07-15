95382ed09a71e00ca1460b6546c14b03dec81979
ryuryu333 <risarisa062518@gmail.com>
2026-07-15 21:47:28 +0900

    fmt使用

--- home-manager/module/nixconf.nix
+++ home-manager/module/nixconf.nix
@@ -1,8 +1,15 @@
 {
   nix.registry = {
     templates = {
-      from = { type = "indirect"; id = "templates"; };
-      to = { type = "github"; owner = "ryuryu333"; repo = "nix_template"; };
+      from = {
+        type = "indirect";
+        id = "templates";
+      };
+      to = {
+        type = "github";
+        owner = "ryuryu333";
+        repo = "nix_template";
+      };
     };
   };
 }
