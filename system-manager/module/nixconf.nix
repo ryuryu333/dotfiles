7bee7c9d0ae7ce0b78d401f52ba2a59aeeddffdb
ryuryu333 <risarisa062518@gmail.com>
2026-07-16 21:24:41 +0900

    nix 背帝を分離

--- system-manager/module/nixconf.nix
+++ system-manager/module/nixconf.nix
@@ -3,16 +3,6 @@
   config = {
     nixpkgs.hostPlatform = hostPlatform;
 
-    environment = {
-      # Add directories and files to /etc and set their permissions.
-      etc = {
-        # 初回導入時、Nix Installerが生成した空の既存ファイルは手動で削除した。
-        # 設定変更後は `sudo systemctl restart nix-daemon.service` で再読み込みする。
-        "nix/nix.custom.conf".text = ''
-          trusted-substituters = https://cache.numtide.com
-          extra-trusted-public-keys = niks3.numtide.com-1:DTx8wZduET09hRmMtKdQDxNNthLQETkc/yaX7M4qK0g=
-        '';
-      };
-    };
+    environment.etc."nix/nix.custom.conf".text = builtins.readFile ../../app-config/nix/wsl/custom.conf;
   };
 }
