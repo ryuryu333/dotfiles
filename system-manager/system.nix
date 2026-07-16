29044b00654ece534048b66bae27ef1967d174e1
ryuryu333 <risarisa062518@gmail.com>
2026-07-16 20:11:26 +0900

    コメント削除

--- system-manager/system.nix
+++ system-manager/system.nix
@@ -3,17 +3,7 @@
   config = {
     nixpkgs.hostPlatform = "x86_64-linux";
 
-    # Enable and configure services.
-    services = {
-      # nginx.enable = true;
-    };
-
     environment = {
-      # Packages that should be installed on the system.
-      systemPackages = [
-        # pkgs.hello
-      ];
-
       # Add directories and files to /etc and set their permissions.
       etc = {
         # 初回導入時、Nix Installerが生成した空の既存ファイルは手動で削除した。
@@ -22,41 +12,7 @@
           trusted-substituters = https://cache.numtide.com
           extra-trusted-public-keys = niks3.numtide.com-1:DTx8wZduET09hRmMtKdQDxNNthLQETkc/yaX7M4qK0g=
         '';
-
-        # with_ownership = {
-        #   text = ''
-        #     This is just a test!
-        #   '';
-        #   mode = "0755";
-        #   uid = 5;
-        #   gid = 6;
-        # };
-        #
-        # with_ownership2 = {
-        #   text = ''
-        #     This is just a test!
-        #   '';
-        #   mode = "0755";
-        #   user = "nobody";
-        #   group = "users";
-        # };
       };
-    };
-
-    # Enable and configure systemd services.
-    systemd.services = { };
-
-    # Configure systemd tmpfile settings.
-    systemd.tmpfiles = {
-      # rules = [
-      #   "D /var/tmp/system-manager 0755 root root -"
-      # ];
-      #
-      # settings.sample = {
-      #   "/var/tmp/sample".d = {
-      #     mode = "0755";
-      #   };
-      # };
     };
   };
 }
