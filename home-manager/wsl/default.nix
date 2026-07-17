f3a40190f93078a2efbc91ba3cf14e4887cf192e
ryuryu333 <risarisa062518@gmail.com>
2026-07-17 18:27:42 +0900

    mac wsl 共通項目を移動

--- home-manager/wsl/default.nix
+++ home-manager/wsl/default.nix
@@ -1,13 +1,5 @@
+{ ... }:
 {
-  pkgs,
-  user,
-  home,
-  ...
-}:
-{
-  home.username = user;
-  home.homeDirectory = home;
-
   home.sessionVariables = {
     DOCKER_HOST = "unix:///run/user/1000/docker.sock";
   };
