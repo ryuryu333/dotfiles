b7b8e8d3a2a7874ffff15b4ffc9b5b55845ce7f1
ryuryu333 <risarisa062518@gmail.com>
2026-07-17 13:54:45 +0900

    system manager に責務委譲

--- home-manager/wsl/default.nix
+++ home-manager/wsl/default.nix
@@ -6,14 +6,4 @@
   home.sessionVariables = {
     DOCKER_HOST = "unix:///run/user/1000/docker.sock";
   };
-
-  # 将来的には system manager で起動シェルを zsh にしたい
-  home.file.".profile".text = ''
-    if [[ $- == *i* ]] \
-      && [[ "''${SHLVL:-0}" -eq 1 ]] \
-      && [[ -n "''${WSL_DISTRO_NAME:-}" ]] \
-      && [[ -x "$HOME/.nix-profile/bin/zsh" ]]; then
-      exec "$HOME/.nix-profile/bin/zsh"
-    fi
-  '';
 }
