45c5f35fd61ad5175381b9157a7bec3b4f9c6f80
ryuryu333 <135518701+ryuryu333@users.noreply.github.com>
2026-07-19 16:19:44 +0900

    pj追加

--- infra/gcp/projects/projects.tf
+++ infra/gcp/projects/projects.tf
@@ -6,6 +6,12 @@ locals {
       auto_create_network = true
       deletion_policy     = "PREVENT"
     }
+    certain-song = {
+      name                = "My Project 82587"
+      billing_account     = null
+      auto_create_network = true
+      deletion_policy     = "PREVENT"
+    }
   }
 }
 
@@ -14,7 +20,7 @@ resource "google_project" "projects" {
 
   name                = each.value.name
   project_id          = var.project_ids[each.key]
-  billing_account     = var.billing_account_ids[each.value.billing_account]
+  billing_account     = each.value.billing_account == null ? null : var.billing_account_ids[each.value.billing_account]
   auto_create_network = each.value.auto_create_network
   deletion_policy     = each.value.deletion_policy
 }
